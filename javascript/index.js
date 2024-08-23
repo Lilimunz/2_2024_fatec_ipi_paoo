3879dfb480bae4b2cc271a77cd4dd0cd68d9b3d0
Alicia Muniz <alicia.muniz@gestaopublica.etc.br>
2024-08-22 22:01:48 -0300

    js: Arrow functions e funções regulares

--- javascript/index.js
+++ javascript/index.js
@@ -1,227 +1,111 @@
+//declaração de variaveis e constantes
 
+//let, const e var (no javascript)
+//const não pode fazer reatribuições
+  // const nome = 'João'
+  // const idade = 19
 
-// GET localhost:8080/livros
-// app.get('/livros', (req, res) => {
+//let da para reatribuir
+  // let nome = 'João'
+  // nome = 'João Silva'
+  // console.log(nome)
 
-// })
+//var um tipo, você não quer usar ele de jeito nenhum
+  //escopo global/retrocompatibilidade
+  //hoist: içamento "leitura dupla" não restringe o escopo da variavel -> só pode usar dentro do if
 
-// POST localhost:8080/livros
-// app.post('/graphql', function(req, res){})
+//objeto JSON - objeto javascript
+//class wrapper - classes empacotadoras
+//null != de undefined
+//null ta definido mas não existe
+//let b = undefined -> algo não definido
 
-//closures
-//função de alta ordem
-// function f (funcao){
-//   console.log('f')
-//   console.log(funcao()())
-//   funcao()()()
-//   return () => console.log('i')
+// dado esse vetor, produzir outro, ele deve conter a letra inicial de cada nome em posição original
+//exercicio proposto em aula
+//[a, j, r, a]
+// const nomes = ['Ana', 'João', 'Rodrigo', 'Angelica'] //map vai iterar sobre qualquer um dos elementos; uma função em função de outra
+// const listaResultante = nomes.map(nome => nome[0]) //primeiro nome, pode ser uma var com nome qualquer
+// // const resultado = []
+//   // for (let i=0; i< nomes.length; i++) {
+//   resultado.push(nomes[i][0])
 // }
+// console.log(listaResultante)
 
-// function g(){
-//   console.log('g')
-//   return function(){
-//     console.log('Fui criada por g')
-//     return () => {console.log('h'); return () => 'k'}
-//   }
+//ex: const = [1, 2, 3]
+//const dobros = numeros.map(n => 2 * n)
+
+//dado esse vetor, construa outro. Ele deve conter apenas os nomes que começão com a letra A
+//const nomes = ['Ana', 'João', 'Rodrigo', 'Angelica']
+
+//forma declarativa (por baixo dos panos, parecido com sql)
+  // const resultado = nomes.filter((n) => {return n[0] === 'A'}) //função como parametro para outra
+  // console.log(resultado)
+
+//Forma imperativa
+  // const resultado =[]
+  // for (let i=0; i< nomes.length; i++){ 
+  //     if (nomes[i][0] === 'A')
+  //         resultado.push(nomes[i]) //push = adicionar
+  // }
+  // console.log(resultado)
+
+  //21 agosto 2024
+  //verifica se pelo menos um começa com a
+  // const nomes = ['Ana', 'Antonio', 'Alex']
+  // const resultado = nomes.some(n => n[0] === 'A' || n[0] === 'a')
+  // console.log(resultado)
+
+  //every
+  //const resultado = nomes
+
+  // const numeros = [1, 2, 3, 4]
+  // const soma = numeros.reduce((ac,V) => ac + V) //recebe dois valores - transforma dois em um só
+  // console.log(soma)
+  //tem um acumulador, tem a coleção de numeros
+  //ac = acumulador, começa valendo 1 (primeiro elemento), depois ele vale 3
+  //V numero da vez, q eh o 2
+
+//arrow functions (=>) funções regulares(function)
+//parametros com valor padrão se não for definido 
+// const triplo = function(n = 5){
+//   return 3 * n
 // }
+// console.log(triplo(6))
+// console.log(triplo()) //ou con undefined entre ()
+//  const dobro = function(n){ //função anonima - pode usar const ou let
+//   return 2 * n
+//  }
+//  console.log(dobro(3))
 
-// f(g())()
-// let umaFuncao = function(){
-//   console.log('sou uma função')
+// function somar(a, b){
+//   return a + b
 // }
-// umaFuncao()
-// public class Pessoa{
-//   public int andar(){return 1;}
-
-//   public void teste(){
-//     // var meuMetodo = andar;
-//     this.andar(correr)
-//   }
-
-//   public void correr(){
-//     return this.andar;
-//   }
+// console.log(somar (1, 2))
+// function hello(){ //ta dizendo só que existe, uma declaração
+//   console.log('oi') //corpo
 // }
-
-// const a = () => console.log('a')
-// const b = () => {
-//   console.log('b')
-//   return 'b'
+// hello()
+// function hello(nome) {
+//   console.log('Oi,' + nome)
 // }
+// hello('Ana')
 
-
-// //closures
-
-
-// // // //funções regulares e arrow functions
-
-// // // // const ehPar = n => (n % 2 === 0)
-
-// // // const f = () => {
-// // //   (return)erro de sintaxe
-// // //   (2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 )
-// // // }
-
-// // const f = a => 2
-// // console.log(f())
-
-// // // const dobro = n => return n * 2
-// // // console.log(dobro(5))
-// // // const hello = () => console.log('Hello')
-// // // hello()
-// // // const helloNome = (nome, idade) => console.log('Oi, ' + nome)
-
-
-// // //parâmetros com valor padrão
-// // // () => {}
-// // // const triplo = function(n = 5){
-// // //   return 3 * n
-// // // }
-// // // console.log(triplo(6))
-// // // console.log(triplo(undefined))
-
-// // //função regular anônima
-// // // const dobro = function(n){
-// // //   return 2 * n
-// // // }
-// // // console.log(dobro(3))
-// // // function somar(a, b){
-// // //   return a + b
-// // // }
-// // // console.log(somar(1, 2))
-
-// // // function hello(){
-// // //   console.log('oi')
-// // // }
-// // // hello()
-// // // function hello(nome){
-// // //   console.log('Oi, ' + nome)
-// // // }
-// // // hello('Ana')
-
-
-// // // const numeros = [1, 2, 3, 4]
-// // // const soma = numeros.reduce((ac, v) => ac + v)
-// // // console.log(soma)
-// // // const nomes = ['Ana', 'Antonio', 'Alex']
-// // // //verifica se pelo menos um começa com a
-// // // const resultado = nomes.some(n => n[0] === 'A' || n[0] === 'a')
-// // // console.log(resultado)
-
-
-// // //every
-// // // const resultado = nomes.every(n => n[0] === 'A' || n[0] === 'a')
-// // // console.log(resultado)
-
-
-// // // let m = 1
-// // // const numeros = [1, 2, 3]
-// // // console.log(numeros.map((valor, indice, numeros) => valor * (indice + 2)))
-
-
-// // //Dado esse vetor, produzir outro. Ele deve conter a letra inicial de cada nome, na sua posição original
-
-// // // const listaResultante = nomes.map(x => x[0])
-
-// // // //[A, J, R, A]
-// // // const nomes = ['Ana', 'João', 'Rodrigo', 'Angelica']
-// // // const resultado = []
-// // // console.log(resultado)
-
-// // // //Dado esse vetor, construa outro. Ele deve conter apenas os nomes que começam com a letra A.
-// // // // const nomes = ['Ana', 'João', 'Rodrigo', 'Angelica']
-// // // // const resultado = nomes.filter(n => {return n[0] === 'A'})
-// // // // console.log(resultado)
-// // // // SELECT * FROM tb_pessoa WHERE idade >= 18
-// // // // const resultado = []
-// // // // for(let i = 0; i < nomes.length; i++){
-// // // //   if (nomes[i[0]] === 'A')
-// // // //     resultado.push(nomes[i]) 
-// // // // }
-// // // console.log(resultado)
-
-// // // //vetores
-// // // // v1 = []
-// // // // console.log(v1.length)
-// // // // v1[0] = 'abc'
-// // // // console.log(v1.length)
-// // // // v1[10] = 2
-// // // // console.log(v1.length)
-// // // // console.log(v1)
-// // // // for(let i = 0; i < v1.length; i++)
-// // // //   console.log(v1[i])
-// // // //comparação por igualdade: == e ===
-// // // // console.log(1 == 1)
-// // // // console.log(1 === 1)
-// // // // console.log(1 == '1')
-// // // // console.log (1 === '1')
-// // // // console.log (true === 1)
-// // // // console.log (true == 1)
-// // // // console.log (1 == [1])
-// // // // console.log(1 == [])
-// // // // console.log(1 == [0])
-// // // // console.log(1 == [0, 1])
-// // // // console.log(1 == [1, 0])
-// // // // console.log(3 == [3])
-// // // // console.log(null == null)
-// // // // console.log(null === undefined)
-// // // // console.log(null == undefined)
-// // // // console.log([] == false)
-// // // // console.log([] == [])
-// // // // console.log(null == 0)
-// // // // console.log(false == 0)
-
-// // // //coerção implícita e explícita
-// // // // const n1 = 2
-// // // // const n2 = '3'
-// // // // const n3 = n1 + n2
-// // // // console.log(n3)
-// // // // const n4 = n1 + Number(n2)
-// // // // console.log(n4)
-
-// // // //declaração de variáveis e constantes
-// // // // let a = null
-// // // // let b = undefined
-// // // // var idade = 18
-// // // // let nome = 'Ana'
-// // // // console.log ('Você tem ' + idade + ' anos')
-// // // // console.log('Oi, ' + nome)
-// // // // //hoist: içamento
-// // // // if(idade >= 18){
-// // // //   console.log(nome + ', você pode dirigir')
-// // // // }
-// // // // console.log('Tchau, ' + nome)
-// // // // var linguagem = 'javascript'
-// // // // var linguagem = 'java'
-// // // // const pi = 3.14
-// // // // pi = 3.141
-// // // //let, const e var
-// // // // let nome = 'João'
-// // // // nome = 'João Silva'
-// // // // console.log(nome)
-// // // // const n = 1
-// // // // n++ // n = n + 1
-// // // // const nome = 'João'
-// // // // //nome = 'João Silva'
-// // // // const idade = 19
-
-// // // //js usa sistema de tipos dinâmico
-// // // //java usa sistema de tipos estático
-// // // //sistema de tipos estático:
-// // // //java, c, c++
-// // // //sistema de tipos dinâmico:
-// // // //javascript, python, php, perl, ruby
-// // // //sistema de tipos gradual
-// // // //C#, Dart, Typescript, Kotlin
-// // // // a = "abc"
-// // // // a.toUppercase()
-// // // // int a;
-// // // // a = 2;
-// // // // a = "abc";
-// // // // a.falar();
-// // // // String s = "abc";
-// // // // s.toUppercase();
-// // // // s.falar();
-
-// // // // let a: number = 2
-// // // // a.falar()
+///Arrow function - não tem nome =>
+//quando tem só um parametro nçao precisa de 
+//quando tem só um console/funçõa não precisa de chaves -- quando se tem um parametro apenas
+//ex:
+//  const hello = () => console.log('Hello')
+// hello()
+// const helloNome = nome => console.log('Oi' + nome) //função que recebe uma outra função como parametro 'nome' eh função depois de helloNome
+//  const dobro = n => {return n * 2} //eh valida mas precisa do return se tiver {}
+//  console.log(dobro(5))
+ //return implicito, desde q não tenha chaves e não tenha mais de uma funçãpo
+// const ehPar = n => {
+//   n % 2 === 0
+// } //falta o return
+// const ehPar = n => (n % 2 === 0)
+// const f = () => {
+//   //(return) //tem um ; nunca vai chegar no 2
+//   //2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 2
+// }
+// console.log(f())
